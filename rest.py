3222c56e959729cb88b86c4a6d6361eaf419b3c4
Anuj Shah <anuj.shah.94@gmail.com>
2016-11-09 15:21:14 -0500

    Update rest.py

--- rest.py
+++ rest.py
@@ -2,11 +2,14 @@
 """
 Created on Mon Oct 31 11:24:05 2016
 
-@author: arpit51187
+@author: Anuj Shah
 """
 
 from flask import Flask
 from flask_restful import Resource, Api
+from flask import Response
+from flask import request
+import xml.etree.ElementTree as ET
 import math
 
 app = Flask(__name__)
@@ -36,6 +39,17 @@ class checkPrime(Resource):
              if number % divisor == 0:
                  return {"prime" : "False"}
          return {"prime" : "True"}
+         
+class apiRSDL(Resource):
+    def get(self):
+        y = '<?xml version="1.0" encoding="UTF-8" ?><rsdl><description>This webservice lets you find the fibonacci series of the number which user has passed. User can also find facotial of the number and user can check weather the passed number is prime or not. This webservice will accept input as a integer values and gives ouput in JSON format. User has to pass parameter in the url itself. This web service uses path parameters.</description><version major="1" minor="0" build="0" revision="12" /><links><link rel="get" href="/fibonacci/number"><request><url><parameters_set><parameter type="xs:int" required="true"><name>Number</name><value>Number of element wanted in fibonacci series.</value></parameter></parameters_set></url></request><response><type>JSON containing result</type></response></link><link rel="get" href="/factorial/number"><request><url><parameters_set><parameter type="xs:int" required="true"><name>number</name><value>Number of which the factorial is needed by user.</value></parameter></parameters_set></url></request><response><type>JSON containing result</type></response></link><link rel="get" href="/checkPrime/number"><request><url><parameters_set><parameter type="xs:int" required="true"><name>number</name><value>Number which user wants to check weather its prime or not.</value></parameter></parameters_set></url></request><response><type>JSON containing result</type></response></link></links></rsdl>'
+        rsdl = request.args.get('rsdl')
+        if(rsdl == ""):
+            x = 'rsdl.xml'
+            #y = ET.parse(x)
+            
+            return Response(y, mimetype='text/xml')        #print(BeautifulSoup(x, "xml").prettify())         
+        return Response(y, mimetype='text/plain') 
         
 class generateFibo(Resource):
     def get(self,number):
@@ -57,6 +71,7 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(generateFibo, '/fibonacci/<int:number>')
 api.add_resource(generateFact, '/factorial/<int:number>')
 api.add_resource(checkPrime, '/checkPrime/<int:number>')
+api.add_resource(apiRSDL, '/api')
 
 
 if __name__ == '__main__':
